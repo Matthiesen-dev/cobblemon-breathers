76a34ec2d3c091626899c4a293f5f5dbad2f25b9
Adam Matthiesen <amatthiesen@outlook.com>
2026-07-25 02:07:24 -0700

    Refactor CobblemonBreathersNeoForgeClient to use IEventBus for event registration

--- neoforge/src/main/java/dev/matthiesen/cobblemon_breathers/neoforge/CobblemonBreathersNeoForgeClient.java
+++ neoforge/src/main/java/dev/matthiesen/cobblemon_breathers/neoforge/CobblemonBreathersNeoForgeClient.java
@@ -3,8 +3,7 @@ package dev.matthiesen.cobblemon_breathers.neoforge;
 import dev.matthiesen.cobblemon_breathers.common.CobblemonBreathers;
 import dev.matthiesen.cobblemon_breathers.common.CobblemonBreathersClient;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
@@ -12,15 +11,12 @@ import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 public final class CobblemonBreathersNeoForgeClient {
     private static final CobblemonBreathersClient instance = CobblemonBreathersClient.INSTANCE;
 
-    public CobblemonBreathersNeoForgeClient() {
+    public CobblemonBreathersNeoForgeClient(IEventBus modBus) {
         instance.initialize();
+        modBus.addListener(this::registerRenderers);
     }
 
-    @EventBusSubscriber(value = Dist.CLIENT, modid = CobblemonBreathers.MOD_ID)
-    public static class ClientEventBus {
-        @SubscribeEvent
-        public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
-            instance.registerRenderers();
-        }
+    public void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
+        instance.registerRenderers();
     }
 }
